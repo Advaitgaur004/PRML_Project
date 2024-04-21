ce307e1d127ad3a43879b81a45f918aece41bc1e
Advait <126783821+Advaitgaur004@users.noreply.github.com>
2024-04-21 21:10:24 +0530

    Update YouTube component to display a spotlight video in PRML_web_page-main/src/component

--- PRML_web_page-main/src/component/Youtube.jsx
+++ PRML_web_page-main/src/component/Youtube.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Youtube() {
   return (
     <div>
-      <p className='header_Problem'>Working Video</p>
+      <p className='header_Problem'>Spolight Video</p>
 
-      <iframe  className='youtube_video'  src="https://www.youtube.com/embed/nvAL3QEGPf0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe  className='youtube_video'  src="https://www.youtube.in/embed/lCZSry91v_Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
   )
 }
